d358e7335b0e47bbc451996e39d28751ab766f1e
Lukas Rytz <lukas.rytz@epfl.ch>
2013-05-06 15:29:34 +0200

    another test

---
+++ tests/src/test/resources/scala/tools/nsc/effects/purity/PuritySuite-files/list.scala
@@ -0,0 +1,20 @@
+import annotation.effects._
+
+abstract class MList[T]
+
+class MNil[T] extends MList[T]
+
+class MCons[T](initX: T, initNext: MList[T]) extends MList[T] {
+  @local var next: MList[T] = initNext
+  @local var x: T = initX
+}
+
+object t {
+  def setHead[T](l: MList[T], newHead: T): Unit @mod(l, newHead) = l match {
+    case _: MNil[_] =>
+      ()
+    case c: MCons[T] =>
+      // needs effect cast because variable c has locality c, we don't support pattern bindings yet.
+      (c: @loc(l)).x = newHead
+  }
+}
